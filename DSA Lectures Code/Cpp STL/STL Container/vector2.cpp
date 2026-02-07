5fe8e4d14b29e5500b440b26f3a870d0d641050f
M Mudasir Iqbal <mr.mudasiriqbal.developer@gmail.com>
2026-02-07 19:12:33 +0500

    Refactor code structure for improved readability and maintainability

--- DSA Lectures Code/Cpp STL/STL Container/vector2.cpp
+++ DSA Lectures Code/Cpp STL/STL Container/vector2.cpp
@@ -2,5 +2,17 @@
 #include<vector>
 using namespace std;
 int main () {
+    vector<int> vec = {1, 2, 3, 4, 5};
 
+    // erase the element from the vector.
+    // vec.erase(vec.begin() + 1, vec.begin() + 3);
+
+    // insert the element in the vector.
+    vec.insert(vec.begin() + 2, 10);
+
+
+    for (int i = 0; i < vec.size(); i++) {
+        cout << vec[i] << " ";
+    }
+    cout << endl;
 }
