971dec51736b49369599765ca29493003072e230
M Mudasir Iqbal <mr.mudasiriqbal.developer@gmail.com>
2026-07-22 15:54:14 +0500

    Update terminal instructions in recursion1.cpp for clarity

--- DSA Lectures Code/Recursion/recursion1.cpp
+++ DSA Lectures Code/Recursion/recursion1.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;   
 
+// to the run the code in the terminal use these commonds.
+// 1rst: g++ <file name>.cpp -o <file name>.exe  
+// press enter and then write the 2nd commond in the terminal
+// 2nd: .\<file name>.exe
+
 void print(int n){
     if(n==0){
         return;
